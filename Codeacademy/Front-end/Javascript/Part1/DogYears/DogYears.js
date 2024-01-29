4cdff5b11382f4971c51751f55d2f8ea91cd5392
MatteoLuca <imazzrecords@gmail.com>
2024-01-29 14:02:07 +0100

    Add DogYears Project

---
+++ Codeacademy/Front-end/Javascript/Part1/DogYears/DogYears.js
@@ -0,0 +1,26 @@
+// Task 1: Create a variable named myAge and set it equal to your age as a number.
+const myAge = 31; // Replace 31 with your actual age
+
+// Task 2: Create a variable named earlyYears and save the value 2 to it.
+let earlyYears = 2;
+
+// Task 3: Use the multiplication assignment operator to multiply earlyYears by 10.5 and reassign it.
+earlyYears *= 10.5;
+
+// Task 4: Subtract 2 from myAge and set the result equal to laterYears.
+let laterYears = myAge - 2;
+
+// Task 5: Multiply laterYears by 4 to calculate the number of dog years accounted for by your later years.
+laterYears *= 4;
+
+// Task 6: Print earlyYears and laterYears to the console to check values.
+console.log(earlyYears);
+console.log(laterYears);
+
+// Task 7: Add earlyYears and laterYears together and store it in myAgeInDogYears.
+const myAgeInDogYears = earlyYears + laterYears;
+
+// Task 8: Write your name as a string, convert it to lowercase, and store it in myName.
+const myName = 'Matteo'.toLowerCase(); 
+// Task 9: Print your name and age in dog years using string interpolation.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
