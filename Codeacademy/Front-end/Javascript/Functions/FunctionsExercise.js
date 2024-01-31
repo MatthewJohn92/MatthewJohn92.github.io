69130ed03264763f17f011743c121a707c38af6e
MatteoLuca <imazzrecords@gmail.com>
2024-01-31 18:19:11 +0100

    add

--- Codeacademy/Front-end/Javascript/Functions/FunctionsExercise.js
+++ Codeacademy/Front-end/Javascript/Functions/FunctionsExercise.js
@@ -9,3 +9,11 @@ function makeShoppingList(item1='milk', item2='bread', item3='eggs'){
 
 
 makeShoppingList('tacos');
+
+// Return - When a function is called, the computer will run through the function’s code and evaluate the result. By default, the resulting value is undefined.
+function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  const numOfMonitors = monitorCount(5,4);
+  console.log(numOfMonitors);
