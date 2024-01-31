793ee85cc410d69af95018e73da6716f9b6a5121
MatteoLuca <imazzrecords@gmail.com>
2024-01-31 18:11:09 +0100

    changed folder and added function exercise

---
+++ Codeacademy/Front-end/Javascript/Functions/FunctionsExercise.js
@@ -0,0 +1,11 @@
+/*
+Default Parameters
+*/
+function makeShoppingList(item1='milk', item2='bread', item3='eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+
+
+makeShoppingList('tacos');
