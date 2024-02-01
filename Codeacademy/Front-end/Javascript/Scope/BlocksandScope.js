9528b94951cbcfe0aff15cf756f30b0ab558a981
MatteoLuca <imazzrecords@gmail.com>
2024-02-01 17:33:30 +0100

    add comments to scope

--- Codeacademy/Front-end/Javascript/Scope/BlocksandScope.js
+++ Codeacademy/Front-end/Javascript/Scope/BlocksandScope.js
@@ -39,3 +39,33 @@ function callMyNightSky() {
   return 'Night Sky: '+ satellite+', ' + stars + ', and ' + galaxy;
 }
 console.log(callMyNightSky());
+
+/*const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  // Add if statement here:
+  
+  if(region==='The Arctic') {
+let lightWaves = 'Northern Lights';
+console.log(lightWaves);
+  }
+  else{
+  console.log(lightWaves);
+  }
+};
+
+logVisibleLightWaves(); 
+
+
+
+
+Let’s review the following terms:
+
+Scope refers to where variables can be accessed throughout the program, and is determined by where and how they are declared.
+Blocks are statements that exist within curly braces {}.
+Global scope refers to the context within which variables are accessible to every part of the program.
+Global variables are variables that exist within global scope.
+Block scope refers to the context within which variables are accessible only within the block they are defined.
+Local variables are variables that exist within block scope.
+Global namespace is the space in our code that contains globally scoped information.
+Scope pollution is when too many variables exist in a namespace or variable names are reused.*/
