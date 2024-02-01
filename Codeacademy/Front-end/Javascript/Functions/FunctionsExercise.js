fb9accdb07f70df1d05ed6a5a1ccb60e6501d8e7
MatteoLuca <imazzrecords@gmail.com>
2024-02-01 12:04:01 +0100

    added anonymous fnction to variable

--- Codeacademy/Front-end/Javascript/Functions/FunctionsExercise.js
+++ Codeacademy/Front-end/Javascript/Functions/FunctionsExercise.js
@@ -15,5 +15,23 @@ function monitorCount(rows, columns) {
     return rows * columns;
   }
   
+  /*
+  Let’s say we have a plant that we need to water once a week on Wednesdays. We could define a function expression to help us check the day of the week and if the plant needs to be watered:
+
+Create a variable named plantNeedsWater using the const variable keyword.
+Assign an anonymous function that takes in a parameter of day to plantNeedsWater.*/
   const numOfMonitors = monitorCount(5,4);
   console.log(numOfMonitors);
+
+
+  const plantNeedsWater = function(day) {
+    if(day === 'Wednesday'){
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  plantNeedsWater('Tuesday');
+  
+  console.log(plantNeedsWater('Tuesday'));
