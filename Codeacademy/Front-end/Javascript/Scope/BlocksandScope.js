32e7245cd5cf37f68ccae1e8f4d136cd1ac9aa87
MatteoLuca <imazzrecords@gmail.com>
2024-02-01 16:37:48 +0100

    added global scope example

--- Codeacademy/Front-end/Javascript/Scope/BlocksandScope.js
+++ Codeacademy/Front-end/Javascript/Scope/BlocksandScope.js
@@ -29,3 +29,13 @@ const logCitySkyline = () => {
 };
 
 console.log(logCitySkyline());
+
+//Global Scope
+
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+function callMyNightSky() {
+  return 'Night Sky: '+ satellite+', ' + stars + ', and ' + galaxy;
+}
+console.log(callMyNightSky());
